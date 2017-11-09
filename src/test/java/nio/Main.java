3f43d61ceeca5dec23f0bbfa5713727ba9d4c096
mymailmjj <mymailmjj@yeah.net>
2017-11-09 18:05:23 +0800

    11月9日上班

--- src/test/java/nio/Main.java
+++ src/test/java/nio/Main.java
@@ -1,47 +1,36 @@
 package nio;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.util.Iterator;
-import java.util.Set;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		
 		try {
-			FileInputStream fileInputStream = new FileInputStream("");
-
-			FileChannel channel = fileInputStream.getChannel();
-
-			ByteBuffer allocate = ByteBuffer.allocate(1024);
-
-			Selector selector = Selector.open();
-
-			Set<SelectionKey> selectedKeys = selector.selectedKeys();
-
-			Iterator<SelectionKey> iterator = selectedKeys.iterator();
-
-			while (iterator.hasNext()) {
-				
-				SelectionKey key = iterator.next();
-
-				if (key.isAcceptable()) {
-
-				} else if (key.isConnectable()) {
-					
-				} else if (key.isReadable()) {
-					
-				} else if (key.isWritable()) {
-					
+			FileInputStream inputStream  = new FileInputStream(new File("C:\\Users\\mujjiang\\Desktop\\0001\\test.txt"));
+			
+			FileChannel channel = inputStream.getChannel();
+			
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			
+			int read = channel.read(buffer);
+			
+			while(read!=-1){
+				buffer.flip();
+				while(buffer.hasRemaining()){
+					byte b = buffer.get();
+					System.out.println((char)b);
 				}
+				
+				buffer.compact();
+				read = channel.read(buffer);
 			}
-
+			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
