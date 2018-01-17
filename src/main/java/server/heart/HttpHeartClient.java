a05e3850ab301d9f92f355bfc937afa37da5e558
mujianjiang <mymailmjj@yeah.net>
2018-01-17 15:47:10 +0800

    heart beat

--- src/main/java/server/heart/HttpHeartClient.java
+++ src/main/java/server/heart/HttpHeartClient.java
@@ -19,95 +19,21 @@ public class HttpHeartClient {
 	private int port;
 
 	private CountDownLatch countDownLatch = new CountDownLatch(1);
-	
+
 	public HttpHeartClient(int port) {
 		this.port = port;
 		init();
 	}
 
-	private void init() {
-
-		try {
-			socketChannel = SocketChannel.open();
-			socketChannel.configureBlocking(false);
-			socketChannel.connect(new InetSocketAddress("localhost", port));
-
-			if (socketChannel.finishConnect()) {
-				
-				countDownLatch.countDown();
-				
-				Thread thread = new Thread(new ServerIdleCheck());
-				
-				thread.setDaemon(true);
-				
-				thread.start();
-			
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	public void send(String str) {
-		
-		try {
-			countDownLatch.await();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			TimeUnit.SECONDS.sleep(10);
-		} catch (InterruptedException e1) {
-			e1.printStackTrace();
-		}
-		
-		byte[] bytes = str.getBytes();
-		
-		ByteBuffer buffer = ByteBuffer.allocate(bytes.length);
-		
-		buffer.put(bytes);
-		
-		buffer.flip();
-		
-		try {
-			socketChannel.write(buffer);
-			System.out.println("消息已经发送");
-			socketChannel.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-
-	}
-
-	private SocketChannel socketChannel;
-
-	class ServerIdleCheck implements Runnable {
-		
-		Thread currentThread = Thread.currentThread();
-
-		ByteBuffer buffer = ByteBuffer.allocate(1024);
+	class HeartBeat implements Runnable{
 
 		public void run() {
 			
-			int i = 0;
-
-			while (true) {
-
+			while(true){
+				
+				ByteBuffer buffer = ByteBuffer.allocate(1024);
+				
 				try {
-					String info = "I'm information from client" + i
-							+ "\t threadname:" + currentThread.getName();
-					buffer.put(info.getBytes());
-					buffer.flip();
-					socketChannel.write(buffer);
-
-					buffer.clear();
-
-					// 每隔三秒钟发送一次
-					TimeUnit.SECONDS.sleep(2);
-
 					long bytesRead = socketChannel.read(buffer);
 					while (bytesRead > 0) {
 						buffer.flip();
@@ -122,8 +48,124 @@ public class HttpHeartClient {
 					buffer.clear();
 				} catch (IOException e) {
 					e.printStackTrace();
+					try {
+						socketChannel.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+					break;
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	private void init() {
+
+		try {
+			socketChannel = SocketChannel.open();
+			socketChannel.configureBlocking(false);
+			socketChannel.connect(new InetSocketAddress("localhost", port));
+
+			if (socketChannel.finishConnect()) {
+
+				countDownLatch.countDown();
+
+				Thread thread = new Thread(new ServerIdleCheck());
+
+				thread.setDaemon(true);
+
+				thread.start();
+				
+				Thread thread1 = new Thread(new HeartBeat());
+
+				thread1.setDaemon(true);
+
+				thread1.start();
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void send(String str) {
+
+		try {
+			countDownLatch.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+
+		byte[] bytes = str.getBytes();
+
+		ByteBuffer buffer = ByteBuffer.allocate(bytes.length);
+
+		buffer.put(bytes);
+
+		buffer.flip();
+
+		try {
+			socketChannel.write(buffer);
+			System.out.println("消息已经发送");
+			socketChannel.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			TimeUnit.SECONDS.sleep(20);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private SocketChannel socketChannel;
+
+	class ServerIdleCheck implements Runnable {
+
+		Thread currentThread = Thread.currentThread();
+
+		ByteBuffer buffer = ByteBuffer.wrap("client heat beat".getBytes());
+
+		public void run() {
+
+			int i = 0;
+
+			while (true) {
+
+				try {
+					socketChannel.write(buffer);
+					buffer.clear();
+					// 每隔三秒钟发送一次
+					TimeUnit.SECONDS.sleep(2);
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+					
+					try {
+						socketChannel.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+					break;
+					
 				} catch (InterruptedException e) {
 					e.printStackTrace();
+					break;
 				}
 			}
 		}
@@ -134,9 +176,9 @@ public class HttpHeartClient {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+
 		HttpHeartClient httpHeartClient = new HttpHeartClient(8081);
-		
+
 		httpHeartClient.send("abcd");
 
 	}
--- src/main/java/server/heart/HttpHeartServer.java
+++ src/main/java/server/heart/HttpHeartServer.java
@@ -6,42 +6,165 @@ package server.heart;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
+import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author cango
- *
+ * 
  */
 public class HttpHeartServer {
-	
-	
-	
-	class ServerIdleCheck implements Runnable{
+
+	private int port;
+
+	public HttpHeartServer(int port) {
+		this.port = port;
+		init();
+	}
+
+	private Selector selector;
+
+	private void init() {
+		try {
+			ServerSocketChannel serverSocketChannel = ServerSocketChannel
+					.open();
+
+			serverSocketChannel.bind(new InetSocketAddress(port));
+
+			serverSocketChannel.configureBlocking(false);
+
+			selector = Selector.open();
+
+			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+			ServiceWork serviceWork = new ServiceWork();
+
+			Thread t1 = new Thread(serviceWork);
+
+			t1.start();
+
+		} catch (ClosedChannelException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	class ServiceWork implements Runnable {
+
+		public void run() {
+			while (true) {
+
+				try {
+					// 如果没有key，则等待
+					if (selector.select(2000) == 0) {
+						System.out.println("====");
+						continue;
+					}
+
+					// 这里的方法是阻塞的，请求的时候会触发想要的keytype,然后继续往下执行
+					Set<SelectionKey> selectedKeys = selector.selectedKeys();
+
+					Iterator<SelectionKey> iterator = selectedKeys.iterator();
+
+					while (iterator.hasNext()) {
+
+						SelectionKey key = iterator.next();
+						if (key.isAcceptable()) {
+							handleAccept(key);
+						} else if (key.isReadable()) {
+							handleRead(key);
+						} else if (key.isWritable()) {
+							handleWrite(key);
+						} else if (key.isConnectable()) {
+
+						}
+
+						iterator.remove();
+
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+
+	}
+
+	/**
+	 * 心跳器
+	 * 
+	 * @author cango
+	 * 
+	 */
+	class ServerIdleCheck implements Runnable {
+
+		private SocketChannel socketChannel;
+
+		public ServerIdleCheck(SocketChannel socketChannel) {
+			this.socketChannel = socketChannel;
+		}
+
+		ByteBuffer buffer = ByteBuffer.wrap("server heart beat".getBytes());
 
 		public void run() {
 			
-			
-			
+			while(true){
+				
+				System.out.println("服务端写入");
+				try {
+					
+					buffer.put("server heat beat".getBytes());
+					
+					buffer.flip();
+					
+					socketChannel.write(buffer);
+					buffer.compact();
+				} catch (IOException e) {
+					e.printStackTrace();
+					try {
+						socketChannel.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+					
+					break;
+				}
+				
+				try {
+					TimeUnit.SECONDS.sleep(2);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
 		}
-		
+
 	}
-	
+
 	private final static int BUFFER_SIZE = 1024;
 
-	public static void handleAccept(SelectionKey key) throws IOException {
+	public void handleAccept(SelectionKey key) throws IOException {
 		ServerSocketChannel ssChannel = (ServerSocketChannel) key.channel();
 		SocketChannel sc = ssChannel.accept();
-		sc.configureBlocking(false);	
+		sc.configureBlocking(false);
 		sc.register(key.selector(), SelectionKey.OP_READ,
 				ByteBuffer.allocateDirect(BUFFER_SIZE));
+		Thread thread = new Thread(new ServerIdleCheck(sc));
+		thread.setDaemon(true);
+		thread.start();
 	}
 
-	//这里对用户进入的请求进行处理
+	// 这里对用户进入的请求进行处理
 	public static void handleRead(SelectionKey key) throws IOException {
 		SocketChannel sc = (SocketChannel) key.channel();
 		ByteBuffer buf = (ByteBuffer) key.attachment();
@@ -55,20 +178,18 @@ public class HttpHeartServer {
 			buf.compact();
 			bytesRead = sc.read(buf);
 		}
-		
+
 		buf.clear();
-		
-		System.out.println("服务端写入");
-		buf.put("server heart beat".getBytes());
-		buf.flip();
-		sc.write(buf);
-		buf.compact();
-		buf.clear();
-//		sc.close();
-		
-//		sc.register(key.selector(), SelectionKey.OP_WRITE);
-		
-	//  这个代码结束
+
+		if (bytesRead == -1) {
+			sc.close();
+		}
+
+		// sc.close();
+
+		// sc.register(key.selector(), SelectionKey.OP_WRITE);
+
+		// 这个代码结束
 	}
 
 	public static void handleWrite(SelectionKey key) throws IOException {
@@ -87,50 +208,8 @@ public class HttpHeartServer {
 
 	public static void main(String[] args) throws IOException {
 
-		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-
-		serverSocketChannel.bind(new InetSocketAddress(8081));
-
-		serverSocketChannel.configureBlocking(false);
-
-		Selector selector = Selector.open();
-
-		serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-		while (true) {
-
-			// 如果没有key，则等待
-			if (selector.select(2000) == 0) {
-				System.out.println("====");
-				continue;
-			}
-
-			//这里的方法是阻塞的，请求的时候会触发想要的keytype,然后继续往下执行
-			Set<SelectionKey> selectedKeys = selector.selectedKeys();
-
-			Iterator<SelectionKey> iterator = selectedKeys.iterator();
-
-			while (iterator.hasNext()) {
-
-				SelectionKey key = iterator.next();
-				if (key.isAcceptable()) {
-					handleAccept(key);
-				} else if (key.isReadable()) {
-					handleRead(key);
-				} else if (key.isWritable()) {
-					handleWrite(key);
-				} else if (key.isConnectable()) {
-
-				}
-
-				iterator.remove();
-
-			}
-
-		}
+		HttpHeartServer httpHeartServer = new HttpHeartServer(8081);
 
 	}
-
-	
 
 }
